0de667c2c663f74158e77bc69c3518d23482297a
Thomas Caron <thomas.caron@polymtl.ca>
2022-12-04 17:40:58 -0500

    Finished day-1

--- day-1/main.py
+++ day-1/main.py
@@ -1,10 +1,14 @@
 from pathlib import Path
 
-def main(input: str):
-    pass
+def part1(input: str):
+    return max([sum(map(int, elf.split('\n'))) for elf in input.split('\n\n')])
+
+def part2(input: str):
+    return sum(sorted([sum(map(int, elf.split('\n'))) for elf in input.split('\n\n')])[-3:])
 
 if __name__ == "__main__":
     current_path = Path(__file__).parent.absolute()
     with open(current_path / "input.txt", "r") as f:
         input = f.read()
-    print(main(input))
+    print(part1(input))
+    print(part2(input))
